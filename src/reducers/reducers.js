43f67b27601216622475636e9b8f35eb498e421e
JoshHarris85 <joshua.harris1085@gmail.com>
2017-12-13 00:09:51 -0600

    refactor: cleanup commented code, unused code, and add import actions

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,14 +2,14 @@ import { combineReducers } from 'redux'
 
 import {
   ADD_POST,
+  VOTE_POST,
+  EDIT_POST,
+  DELETE_POST,
   ADD_COMMENT,
+  VOTE_COMMENT,
+  EDIT_COMMENT,
+  DELETE_COMMENT,
 } from '../actions/actions'
-
-const initialAppState = {
-  News: {
-    Posts: []
-  }
-}
 
 function posts (state = {}, action) {
   const { title, body, author, category } = action
@@ -35,62 +35,46 @@ function comments (state = {}, action) {
     default :
       return state
   }
-  // const { day, recipe, meal } = action
-  //
-  // switch (action.type) {
-  //   case ADD_RECIPE :
-  //     return {
-  //       ...state,
-  //       [day]: {
-  //         ...state[day],
-  //         [meal]: recipe.label,
-  //       }
-  //     }
-  //   case REMOVE_FROM_CALENDAR :
-  //     return {
-  //       ...state,
-  //       [day]: {
-  //         ...state[day],
-  //         [meal]: null,
-  //       }
-  //     }
-  //   default :
-  //     return state
-  // }
 }
 
-// {
-//   News: {Posts: { {id: 1, comments: {id: 1}} {id: 2, comments: {id: 2}} }},
-//   Programming: {Posts: { {id: 3, comments: {id: 3}} {id: 4, comments {id: 4}} }}
+function categories (state = {}, action) {
+  switch (action.type) {
+    case ADD_COMMENT :
+      return {
+        ...state
+      }
+    default :
+      return state
+  }
+}
+
+// const initialCategoriesState = {
+//   name: null,
+//   url: null
 // }
-
-const initialCategoriesState = {
-  name: null,
-  url: null
-}
-
-const initialPostState = {
-  id: null,
-  timestamp: Date.now(),
-  title: null,
-  body: null,
-  author: null,
-  category: null,
-  voteScore: 1,
-  deleted: false
-}
-
-
-const initialCommentState = {
-  id: null,
-  parentId: null,
-  timestamp: Date.now(),
-  body: null,
-  author: null,
-  voteScore: 1,
-  deleted: false,
-  parentDeleted: null
-}
+//
+// const initialPostState = {
+//   id: null,
+//   timestamp: Date.now(),
+//   title: null,
+//   body: null,
+//   author: null,
+//   category: null,
+//   voteScore: 1,
+//   deleted: false
+// }
+//
+//
+// const initialCommentState = {
+//   id: null,
+//   parentId: null,
+//   timestamp: Date.now(),
+//   body: null,
+//   author: null,
+//   voteScore: 1,
+//   deleted: false,
+//   parentDeleted: null
+// }
 
 export default combineReducers({
   posts,
